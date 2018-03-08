3b236eacc5c88bed063db331b019733b52c2a729
Don Mac <don_macadat@dlsu.edu.ph>
2018-03-09 06:35:27 +0800

    finished inputphysical
    kulang nalang ng checker if tama ung usertype sa header

--- inputphysical.php
+++ inputphysical.php
@@ -32,6 +32,7 @@
                                           while($row = mysqli_fetch_array($sql)){
                                               $rmName[$i] = $row['rmName'];
                                               $rmID[$i] = $row['rmID'];
+                                              $ingID[$i] = $row['ingID'];
                                               $rm = $row['rmID'];
                                               echo "<tr><td>";
                                               echo "<input type ='hidden' name='rmID[]' value='$rm' required>";
@@ -59,55 +60,71 @@
         // if submit button is pressed
         if (isset($_POST['submit'])){
           $message=NULL;
-          // counting all ingredients in the inventory
-          $query1 = mysqli_query($conn,"select count(rmID) as count from inventory");
+          // counting all rawmats in the inventory
+          $query1 = mysqli_query($conn,"select count(ingID) as count from inventory");
           while($row=mysqli_fetch_array($query1)){
               $count = $row['count'];
-              // $count for container ingredient count
+              // $count for container rawmat count in inventory
           }
           // $rmID raw materials id in an array
           $rmID = $_POST['rmID'];
-          // $quantity of raw materials per item in an array
+          // $quantity of raw materials per item in an array as counted physically
           $quantity = $_POST['quantity'];
-          // $date getting the date to track when the physical count is done. Year Month day format
+          // $date getting the date to track when the physical count is done. Year Month day format yyyy-mm-dd
           $date = date('Y-m-d');
           // $userID getting the current user who used the form
           $userID = $_SESSION['userid'];
-
-          $sqlInventory = "SELECT * FROM inventory";
-          $resu = mysqli_query($conn,$sqlInventory);
+          // getting the total from ingredient table
+          $resf = mysqli_query($conn,"SELECT * FROM ingredient");
 
           // checker if there is actual ingredients in the inventory
           if($count > 0)
           {
+             for ($i=0; $i < $count; $i++) {
+               // query for inserting into input physical count.
+               $sql = "INSERT INTO input_physical_count(rmID, userID, quantity, date) VALUES('$rmID[$i]', '$userID', '$quantity[$i]', '$date')";
+               $result = mysqli_query($conn, $sql);
+             }
              // looping through count of all ingredients in inventory table listed in input physical
              for($i=0; $i<$count; $i++)
               {
-                  $rowi=mysqli_fetch_array($resu);
-                  $measurement = $rowi['measurement_value'];
-                  // echo $measurement . "<br />";
-                  // query for inserting into input physical count.
-                  // $sql = "INSERT INTO input_physical_count(rmID, userID, quantity, date) VALUES('$rmID[$i]', '$userID', '$quantity[$i]', '$date')";
-                  // $result = mysqli_query($conn, $sql);  
+                  $resu = mysqli_query($conn,"SELECT * FROM inventory WHERE ingID = '$ingID[$i]'");
+                  $computedvalue = 0;
+                  $rowd = mysqli_fetch_array($resf);
+                  if ($resu->num_rows > 1) {
+                    // for repeating ingID's
+                    for ($j=0; $j < $resu->num_rows; $j++) {
+                      $row = mysqli_fetch_array($resu);
+                      $tempcount = $quantity[$j] * $row['measurement_value'];
+                      $computedvalue = $computedvalue + $tempcount;
+                    }
+                    if ($computedvalue!=$rowd['total']) {
+                      $temp = $computedvalue - $rowd['total'];
+                      $iding = $rowd['ingID'];
+                      $sql = "INSERT INTO discrepancy(ingredientID, discrepancyCount, checkerUser, checkedDate) VALUES('$iding','$temp','$userID','$date')";
+                      $resulta = mysqli_query($conn,$sql);
+                    }
+                    
+                    $i += $resu->num_rows-1;
+                  }
+                  else {
+                    // for non repeating ingID
+                    $row = mysqli_fetch_array($resu);
+                    $computedvalue = $quantity[$i] * $row['measurement_value'];
+                    if ($computedvalue!=$rowd['total']) {
+                      $temp = $computedvalue - $rowd['total'];
+                      $iding = $rowd['ingID'];
+                      $sql = "INSERT INTO discrepancy(ingredientID, discrepancyCount, checkerUser, checkedDate) VALUES('$iding','$temp','$userID','$date')";
+                      $resulta = mysqli_query($conn,$sql);
+                    }
+                  }
 
-
-                  $compareQuantity = $measurement * $quantity[$i];
-                  echo $compareQuantity."<br />";
-
-                  // comparing condition if != inserts into discrepancy table
-                  // if ($compareQuantity != $ingredientTotal) {
-                  //     $temp = $ingredientTotal - $compareQuantity;
-                  //     $ingid = $row2['ingID'];
-                  //     $sql = "INSERT INTO discrepancy(ingredientID, discrepancyCount, checkerUser, checkedDate) VALUES('$ingid','$temp','$userID','$date')";
-                  //     $resulta = mysqli_query($conn,$sql);
-                  // }
               }//end of for loop
 
 
         }//end of if count > 0
 
     }//end of if submit statement
-
     ?>
 
                                 </div>
